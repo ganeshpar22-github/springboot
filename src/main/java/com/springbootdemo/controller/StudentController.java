2ff9062025eb61f5fdafd0977112903daca5a509
Ganesh Parmar <ganeshparmar221997@gmail.com>
2022-03-31 19:34:11 +0530

    [SpringBoot Project]
    Added new method for fetching list of students and pom pump up to 1.0.1

--- src/main/java/com/springbootdemo/controller/StudentController.java
+++ src/main/java/com/springbootdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.springbootdemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,20 @@ public class StudentController {
 	@GetMapping("/student")
 	public Student getStudent() {
 		return new Student("Ganesh", "Parmar", 24, "Dewas");
+	}
+	
+	
+	@GetMapping("/studentlist")
+	public List<Student> getStudentList(){
+			
+		List<Student> students = new ArrayList<>();
+		students.add(new Student("Ganesh", "Parmar", 24, "Dewas"));
+		students.add(new Student("Yash", "Parmar", 21, "Dewas"));
+		students.add(new Student("Aditya", "Namdev", 24, "Pune"));
+		students.add(new Student("Rishabh", "Kundal", 25, "Kolkata"));
+		students.add(new Student("Priyank", "Vyas", 25, "Mumbai"));
+		
+		return students;
 	}
 
 }
